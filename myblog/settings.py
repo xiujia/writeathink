0005a8874ba1e40770b3817d97a486c50b748f88
xiujiaw <xiujiaw@gmail.com>
2017-08-21 10:23:13 +0800

    deploy settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$q$d_yi^j0u-&h-$83ovra+&#gr9ay5=6kk^z&y##t_3oj-k(4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.writeathink.cn']
 
 
 # Application definition
@@ -142,3 +142,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
